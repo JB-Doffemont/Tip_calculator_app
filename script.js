4e933486faa47f9ea02ea80fb840eaead93b9748
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-02 15:56:39 +0100

    commit tipPerPerson + totalPerPerson

--- script.js
+++ script.js
@@ -1,24 +1,103 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Appel des éléments du Dom
-  let bill = document.querySelector("#bill");
-  let percent = document.querySelector("#percent");
-  let person = document.querySelector("#nb_people");
-  let tipAmount = document.querySelector("#tip_amount");
+// Call elements from Dom
+let bill = document.querySelector("#bill");
+let percentAmount = 0;
+let percentFive = document.querySelector("#percent_five");
+let percentTen = document.querySelector("#percent_ten");
+let percentFifteen = document.querySelector("#percent_fifteen");
+let percentTwentyFive = document.querySelector("#percent_twentyFive");
+let percentFifty = document.querySelector("#percent_fifty");
+let person = document.querySelector("#nb_people");
+let tipAmount = document.querySelector("#tip_amount");
+let totalPerson = document.querySelector("#total_person");
 
-  percent.addEventListener("click", () => {
-    console.log(percent.value);
-    tipPerPerson();
-  });
+// Event on click to define the percentage
+percentFive.addEventListener("click", () => {
+  percentAmount = 5;
+  console.log(percentAmount);
+  tipPerPerson(percentAmount);
+});
 
-  function tipPerPerson() {
-    total_tip = (bill.value * percent.innerHTML) / 100;
-    console.log(total_tip);
+percentTen.addEventListener("click", () => {
+  percentAmount = 10;
+  console.log(percentAmount);
+  tipPerPerson(percentAmount);
+});
 
+percentFifteen.addEventListener("click", () => {
+  percentAmount = 15;
+  console.log(percentAmount);
+  tipPerPerson(percentAmount);
+});
+
+percentTwentyFive.addEventListener("click", () => {
+  percentAmount = 25;
+  console.log(percentAmount);
+  tipPerPerson(percentAmount);
+});
+
+percentFifty.addEventListener("click", () => {
+  percentAmount = 50;
+  console.log(percentAmount);
+  tipPerPerson(percentAmount);
+});
+
+// Functions to get tips per person by percentage
+function tipPerPerson() {
+  if (percentAmount == 5) {
     person.addEventListener("input", () => {
+      total_tip = (bill.value * percentFive.innerHTML) / 100;
+      console.log(total_tip);
       tipPerson = total_tip / person.value;
       console.log(tipPerson);
       tipAmount.innerHTML = tipPerson;
-      return tipPerson;
+
+      totalPerPerson();
+    });
+  } else if (percentAmount == 10) {
+    person.addEventListener("input", () => {
+      total_tip = (bill.value * percentTen.innerHTML) / 100;
+      console.log(total_tip);
+      tipPerson = total_tip / person.value;
+      console.log(tipPerson);
+      tipAmount.innerHTML = tipPerson;
+
+      totalPerPerson();
+    });
+  } else if (percentAmount == 15) {
+    person.addEventListener("input", () => {
+      total_tip = (bill.value * percentFifteen.innerHTML) / 100;
+      console.log(total_tip);
+      tipPerson = total_tip / person.value;
+      console.log(tipPerson);
+      tipAmount.innerHTML = tipPerson;
+
+      totalPerPerson();
+    });
+  } else if (percentAmount == 25) {
+    person.addEventListener("input", () => {
+      total_tip = (bill.value * percentTwentyFive.innerHTML) / 100;
+      console.log(total_tip);
+      tipPerson = total_tip / person.value;
+      console.log(tipPerson);
+      tipAmount.innerHTML = tipPerson;
+
+      totalPerPerson();
+    });
+  } else if (percentAmount == 50) {
+    person.addEventListener("input", () => {
+      total_tip = (bill.value * percentFifty.innerHTML) / 100;
+      console.log(total_tip);
+      tipPerson = total_tip / person.value;
+      console.log(tipPerson);
+      tipAmount.innerHTML = tipPerson;
+
+      totalPerPerson();
     });
   }
-});
+}
+
+// Function to get the total amount by person
+function totalPerPerson() {
+  totalPerson.innerHTML = bill.value / person.value + tipPerson;
+  return totalPerson;
+}
